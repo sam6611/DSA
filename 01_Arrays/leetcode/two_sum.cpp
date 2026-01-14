4033cee763cf6b257fb84480140772077e5b7d02
Satyam Sachan <satyamsachan04@gmail.com>
2026-01-14 11:39:06 +0530

    Solved two sum using array

---
+++ 01_Arrays/leetcode/two_sum.cpp
@@ -0,0 +1,20 @@
+// LeetCode 1 - Two Sum
+// Approach: Brute Force (Array)
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
