23dd734eb326806d700beef4ee5eb457be9c4a3a
Satyam Sachan <satyamsachan04@gmail.com>
2026-01-15 15:13:49 +0530

    Create Contains_Duplicate.cpp

---
+++ 01_Arrays/leetcode/Contains_Duplicate.cpp
@@ -0,0 +1,20 @@
+// LeetCode 217 - Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/
+//
+// Approach: Sorting
+// Time Complexity: O(n log n)
+// Space Complexity: O(1) (ignoring sorting space)
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
