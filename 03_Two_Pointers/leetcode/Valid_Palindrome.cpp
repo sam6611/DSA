c2c3c62985cc6305eccdef566331e25c7d2a97a7
Satyam Sachan <satyamsachan04@gmail.com>
2026-01-22 00:04:09 +0530

    Update Valid_Palindrome.cpp

--- 03_Two_Pointers/leetcode/Valid_Palindrome.cpp
+++ 03_Two_Pointers/leetcode/Valid_Palindrome.cpp
@@ -3,29 +3,26 @@
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0;
-        int right = s.size() - 1;
-
-        while (left < right) {
-            if (!isalnum(s[left])) {
+        int left=0,right=s.length()-1;
+        while(left<right){
+            if(!isalnum(s[left])){
                 left++;
-            } 
-            else if (!isalnum(s[right])) {
+            }
+            else if(!isalnum(s[right])){
                 right--;
-            } 
-            else {
-                if (tolower(s[left]) != tolower(s[right])) {
+
+            }
+            else{
+                if(tolower(s[left])!=tolower(s[right])){
                     return false;
                 }
                 left++;
-                right--;
+            right--;
             }
+            
         }
         return true;
     }
