f42cad853d1be1fe08e94a20db4400a6bf133ced
Satyam Sachan <satyamsachan04@gmail.com>
2026-04-11 21:27:43 +0530

    Implement minimumDistance function in miniDistance.cpp

---
+++ leetcode/miniDistance.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        unordered_map<int, vector<int>> mp;
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+        
+        int ans = INT_MAX;
+        
+        for(auto &it : mp) {
+            auto &v = it.second;
+            int m = v.size();
+            
+            if(m < 3) continue;
+            
+            for(int i = 0; i + 2 < m; i++) {
+                ans = min(ans, 2 * (v[i+2] - v[i]));
+            }
+        }
+        
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
