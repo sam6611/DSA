3f89ab1c244aad29f769aae56afeb9e5fa9b9b9e
Satyam Sachan <satyamsachan04@gmail.com>
2026-04-15 23:09:20 +0530

    Create closestTarget.cpp

---
+++ leetcode/closestTarget.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        int n = words.size();
+        int ans = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            if (words[i] == target) {
+                int diff = abs(i - startIndex);
+                int dist = min(diff, n - diff); // circular distance
+                ans = min(ans, dist);
+            }
+        }
+
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
